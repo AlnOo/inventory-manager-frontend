3fa61e0630fa30df550f5711f80bcb65f716d4c0
Allan <github email address>
2022-11-09 08:26:45 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/js/bootstrap";
+import * as bootstrap from 'bootstrap';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import authReducer from './store/reducers/authReducer'
+import mainReducer from './store/reducers/mainReducer'
+import thunk from 'redux-thunk'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    main: mainReducer
+})
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+registerServiceWorker()
